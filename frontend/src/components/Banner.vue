18f3025f7f3b7084ff7e19342b6576f831425769
SeaNguyen <imgosu.nguyen@gmail.com>
2023-05-23 22:02:46 +0700

    update banner, create newsletter

--- frontend/src/components/Banner.vue
+++ frontend/src/components/Banner.vue
@@ -1,24 +1,26 @@
 <template>
-  <div class="content h-full flex flex-col justify-end my-0 mx-auto md:max-w-7xl md:flex-row">
+  <div
+    class="content h-full flex items-center flex-col-reverse max-w-[calc(100%-20px)] relative justify-center  my-0 mx-auto md:max-w-7xl md:flex-row md:justify-end">
     <div
-      class="text-content text-white text-center flex flex-col items-center md:absolute md:left-[50px] md:top-1/2 md:-translate-y-1/2 2xl:left-0 2xl:text-start 2xl:items-start">
+      class="text-content text-white text-center flex flex-col items-center md:absolute md:left-[50px] md:top-1/2 md:-translate-y-1/2 2xl:left-0">
       <h5 class="text-white text-xl uppercase text-bold mb-[20px]">Summer 2023</h5>
-      <h1 class="text-[60px] max-w-[950px] uppercase text-white text-bold leading-none mb-[20px] md:text-[90px]">virtual
+      <h1 class="text-[60px] max-w-[800px] uppercase text-white text-bold leading-none mb-[20px] md:text-[70px]">virtual
         reality glasses</h1>
-      <h4 class="max-w-[300px] text-white text-base mb-[20px] md:max-w-[500px] md:text-lg md:p-0">we know how large objects will
+      <h4 class="max-w-[300px] text-white text-base mb-[20px] md:max-w-[500px] md:text-lg md:p-0">we know how large
+        objects will
         act, but
         things on a small scale</h4>
       <div class="ctas flex items-center gap-5">
         <Button
-          class="uppercase text-xs font-medium border-2 border-solid border-white py-[10px] px-[20px] cursor-pointer">Mua
-          ngay bây giờ</Button>
+          class="uppercase text-xs font-medium border-2 border-solid border-white py-[10px] px-[20px] cursor-pointer">Chi
+          tiết</Button>
         <Button
           class="bg-white text-black uppercase text-xs font-medium border-2 border-solid border-white py-[10px] px-[20px] cursor-pointer">Mua
           ngay bây giờ</Button>
       </div>
     </div>
     <img src="../assets/images/vr-glasses.png"
-      class="relative z-[9] w-[400px] mb-[20px] md:w-[600px] md:mr-[60px] md:mt-[50px] md:mb-0 2xl:w-[700px] 2xl:mr-0 2xl:mt-0"
+      class="relative z-[9] w-[400px] mb-[20px] md:w-[500px] md:mr-[60px] md:mt-[50px] md:mb-0 2xl:w-[600px] 2xl:mr-0 2xl:mt-0"
       alt="">
   </div>
 </template>
--- frontend/src/views/pages/Home.vue
+++ frontend/src/views/pages/Home.vue
@@ -6,17 +6,23 @@
     <Banner />
   </div>
   <!-- <div class="h-[2000px]">home</div> -->
+
+  <div class="newsletter-section">
+    <Newsletter/>
+  </div>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar.vue';
 import Banner from '@/components/Banner.vue';
+import Newsletter from '../../components/Newsletter.vue';
 import { ref } from 'vue'
 export default {
   components: {
     Navbar,
     Banner,
-  },
+    Newsletter,
+},
 
   setup(props, {emit}) {
 
