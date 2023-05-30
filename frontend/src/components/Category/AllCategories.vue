97928e42780ae6a6ae9f0603b3ddcc984627fb82
Hải Ngọc Nguyễn <imgosu.nguyen@gmail.com>
2023-05-30 17:27:22 +0700

    update all categories

---
+++ frontend/src/components/Category/AllCategories.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="my-[30px] md:my-[75px]">
+    <div class="container my-[1.875rem] mx-auto">
+      <Breadcrumb />
+    </div>
+
+    <div class="container overflow-hidden shadow border-[0.125rem] mb-[1.875rem]">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+import Breadcrumb from './Breadcrumb.vue';
+
+export default {
+
+  components: {
+    Breadcrumb
+
+  },
+}
+</script>
+
+<style scoped></style>
---
+++ frontend/src/components/Category/CategoryCard/CategoryCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="">
+    <div class="image">
+      <img src="" alt="">
+    </div>
+
+    <div>Smart Phone</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped></style>
