092f4b4cc1b3029ef5d983be93f5fcf223de788e
Hải Ngọc Nguyễn <imgosu.nguyen@gmail.com>
2023-05-31 14:47:09 +0700

    update header for cart

--- frontend/src/components/Header/Headerv2.vue
+++ frontend/src/components/Header/Headerv2.vue
@@ -1,11 +1,11 @@
 <template>
   <header class="w-full px-[20px] py-4 bg-white text-black shadow md:px-[40px]">
     <div class="flex items-center justify-between h-12 max-w-7xl my-0 mx-auto md:h-[60px]">
-      <router-link :to="{name:'cart'}" class="logo flex items-center gap-2 hover:text-black">
+      <router-link :to="{name:headingLink}" class="logo flex items-center gap-2 hover:text-black">
         <Icon icon="cryptocurrency:nano" class="text-3xl" />
         <h2 class="font-bold text-3xl text-black cursor-pointer">Nanolabs</h2>
         <span class="text-black text-3xl">|</span>
-        <h2 class="font-bold text-2xl text-black cursor-pointer">Giỏ Hàng</h2>
+        <h2 class="font-bold text-2xl text-black cursor-pointer">{{ headingName }}</h2>
       </router-link>
 
       <div class="flex items-center">
@@ -21,6 +21,11 @@ import { Icon } from '@iconify/vue'
 import { ref } from 'vue'
 import Button from '@/components/Button.vue'
 export default {
+  props:{
+    headingLink: String,
+    headingName: String,
+  },
+
   components: {
     Icon,
     Button,
--- frontend/src/views/pages/CartPage.vue
+++ frontend/src/views/pages/CartPage.vue
@@ -1,24 +1,34 @@
 <template>
-  <CartHeader/>
+  <Headerv2 :headingLink="headingLink" :headingName="headingName"/>
 
   <section>
-    <CartSection/>
+    <CartSection />
   </section>
 
-  <Footer/>
+  <Footer />
 </template>
 
 <script>
 import Footer from '@/components/Footer/Footer.vue';
-import CartHeader from '../../components/Cart/CartHeader.vue';
 import CartSection from '../../components/Cart/CartSection.vue';
+import Headerv2 from '../../components/Header/Headerv2.vue';
 
 export default {
   components: {
     Footer,
-    CartHeader,
-    CartSection
-},
+    CartSection,
+    Headerv2
+  },
+
+  setup(props) {
+    const headingLink = "cart";
+    const headingName = "Giỏ Hàng";
+
+    return {
+      headingLink,
+      headingName,
+    }
+  }
 }
 </script>
 
