0d7d6140666a0281554673b4c0ee1e2bd332bfac
Hải Ngọc Nguyễn <imgosu.nguyen@gmail.com>
2023-06-12 14:07:58 +0700

    update sidebar

--- frontend/src/components/Dashboard/DashboardSideBar.vue
+++ frontend/src/components/Dashboard/DashboardSideBar.vue
@@ -1,12 +1,58 @@
 <template>
-  <div>
-    sidevbar
-  </div>
+  <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
+    <a-menu-item key="1">
+      <span>
+        <Icon icon="ic:round-dashboard" />
+      </span>
+      <span>Dashboard</span>
+    </a-menu-item>
+    <a-menu-item key="2">
+      <Icon icon="icon-park-outline:analysis" />
+      <span>Analytics</span>
+    </a-menu-item>
+    <a-menu-item key="3">
+      <Icon icon="solar:box-outline" />
+      <span>Products</span>
+    </a-menu-item>
+    <a-menu-item key="4">
+      <Icon icon="cil:wallet" />
+      <span>Payment</span>
+    </a-menu-item>
+    <a-menu-item key="5">
+      <Icon icon="clarity:truck-line" />
+      <span>Orders</span>
+    </a-menu-item>
+    <a-menu-item key="6">
+      <Icon icon="uil:chat-bubble-user" />
+      <span>Enquiry</span>
+    </a-menu-item>
+    <a-menu-item key="7">
+      <Icon icon="ph:projector-screen-chart-light" />
+      <span>Marketing</span>
+    </a-menu-item>
+    <a-menu-item key="8">
+      <Icon icon="ep:setting" />
+      <span>Setting</span>
+    </a-menu-item>
+  </a-menu>
 </template>
 
 <script>
+import { Icon } from '@iconify/vue';
+import { ref } from 'vue'
 export default {
+  components: {
+		Icon,
+    
+	},
 
+  setup(props) {
+    const selectedKeys = ref(['1']);
+
+    return {
+      selectedKeys,
+    }
+  }
 }
 </script>
 
--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,6 +1,6 @@
 <template>
   <a-layout class="min-h-screen">
-    <a-layout-sider class="bg-white">
+    <a-layout-sider class="bg-white" v-model:collapsed="collapsed" collapsible>
       <DashboardSideBar />
     </a-layout-sider>
 
@@ -13,11 +13,20 @@
 <script>
 import DashboardHome from '@/components/Dashboard/DashboardHome.vue'
 import DashboardSideBar from '@/components/Dashboard/DashboardSideBar.vue'
+import { ref } from 'vue'
 export default {
   components: {
     DashboardHome,
     DashboardSideBar,
   },
+
+  setup(props) {
+    const collapsed = ref(false);
+
+    return {
+      collapsed,
+    }
+  }
 }
 </script>
 
